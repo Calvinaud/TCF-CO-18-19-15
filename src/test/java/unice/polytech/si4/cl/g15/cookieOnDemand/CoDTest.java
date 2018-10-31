72237aab261f5c664f76cc2ec92f7295d8ed4a3d
Calvin Audier <calvin.audier@etu.unice.fr>
2018-10-31 16:26:40 +0100

    Inversion du contenu de CoDTest et RecipeTest

--- src/test/java/unice/polytech/si4/cl/g15/cookieOnDemand/CoDTest.java
+++ src/test/java/unice/polytech/si4/cl/g15/cookieOnDemand/CoDTest.java
@@ -4,22 +4,21 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class CoDTest {
 
-    Recipe recipe;
-    float price;
-
-    @Before
-    public void setUp(){
-        price = 300;
-        String[] topping = {"MM","Chocolate"};
-        recipe = new Recipe("Magical Cookie", "Chocolate", "Vanilla", "Mixed", "Chewy", price, topping);
+    @Test
+    public void getInstanceNotNull() throws Exception {
+        CoD c = CoD.getInstance();
+        assertNotNull(c);
     }
 
     @Test
-    public void getPrice() throws Exception {
-        assertEquals(price,recipe.getPrice());
+    public void getInstanceSame() throws Exception {
+        CoD c = CoD.getInstance();
+        CoD c2 = CoD.getInstance();
+        assertEquals(c, c2);
     }
 
 }
--- src/test/java/unice/polytech/si4/cl/g15/cookieOnDemand/RecipeTest.java
+++ src/test/java/unice/polytech/si4/cl/g15/cookieOnDemand/RecipeTest.java
@@ -8,17 +8,19 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 public class RecipeTest {
-    @Test
-    public void getInstanceNotNull() throws Exception {
-    	CoD c = CoD.getInstance();
-    	assertNotNull(c);
-    }
-    
-    @Test
-    public void getInstanceSame() throws Exception {
-    	CoD c = CoD.getInstance();
-    	CoD c2 = CoD.getInstance();
-    	assertEquals(c, c2);
+
+    Recipe recipe;
+    float price;
+
+    @Before
+    public void setUp(){
+        price = 300;
+        String[] topping = {"MM","Chocolate"};
+        recipe = new Recipe("Magical Cookie", "Chocolate", "Vanilla", "Mixed", "Chewy", price, topping);
     }
 
+    @Test
+    public void getPrice() throws Exception {
+        assertEquals(price,recipe.getPrice());
+    }
 }
