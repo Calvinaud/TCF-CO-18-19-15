143fa09fafc95c1cf14cce0536d96f3e6505ca77
Calvin Audier <calvin.audier@etu.unice.fr>
2018-11-05 17:49:47 +0100

    Modification de la gestion des horraires de magasin. Création de Shop Test

--- src/main/java/unice/polytech/si4/cl/g15/cookieOnDemand/Shop.java
+++ src/main/java/unice/polytech/si4/cl/g15/cookieOnDemand/Shop.java
@@ -1,22 +1,41 @@
 package unice.polytech.si4.cl.g15.cookieOnDemand;
 
+import org.joda.time.LocalTime;
+
 import java.util.*;
 
 public class Shop {
 
-	private Recipe recipe;
-	private Collection<Manager> manager;
+	private Recipe monthlyRecipe;
+	private List<Manager> manager;
 	private float tax;
-	private Date businessHours;
+	private LocalTime openTime;
+	private LocalTime closeTime;
 	private String address;
 	private int phone;
+	private List<Command> commands;
 
-	public Recipe getMonthlyRecipe() {
-		throw new UnsupportedOperationException();
+	public Shop(String adress, float tax, int phone, LocalTime openTime, LocalTime closeTime, Manager... manager){
+		this.address = adress;
+	    this.tax = tax;
+		this.phone = phone;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+        this.commands = new ArrayList<Command>();
+        this.manager = new ArrayList<Manager>();
+        this.manager.addAll(Arrays.asList(manager));
 	}
 
-	public Command[] getCommand() {
-		throw new UnsupportedOperationException();
+	public void changeMonthlyRecipe(String name, String dough, String flavour, String mixed, String cooking, float price, String... topping){
+        monthlyRecipe = new Recipe(name, dough, flavour, mixed, cooking, price, topping);
+    }
+
+	public Recipe getMonthlyRecipe() {
+		return monthlyRecipe;
+	}
+
+	public List<Command> getCommand() {
+		return commands;
 	}
 
 	public void setTax(float tax){
---
+++ src/test/java/unice/polytech/si4/cl/g15/cookieOnDemand/ShopTest.java
@@ -0,0 +1,7 @@
+package unice.polytech.si4.cl.g15.cookieOnDemand;
+
+import static org.junit.Assert.*;
+
+public class ShopTest {
+
+}
